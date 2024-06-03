7510024d46acdbf96c010d951381ab4022dbb785
ALAN TONATIUH <programador.sr@coorsamexico.com>
2024-06-03 09:58:25 -0600

    intento requirements

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from scr.features.screws.screens.screws_screen import ScrewsScreen
 
 if __name__ == "__main__":
     Enviroments.init_environment()
-    app = ScrewsScreen()
-    app.mainloop()
+    #app = ScrewsScreen()
+    #app.mainloop()
